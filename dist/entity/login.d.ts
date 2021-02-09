1de58d3f721f6fe762c14848193bb8e6c12a70ad
Marcos A C de Paula <cardepaula@gmail.com>
2021-02-09 08:57:09 -0300

    refactor: gitignore

--- dist/entity/login.d.ts
+++
@@ -1,5 +0,0 @@
-export declare class Login {
-    id: number;
-    email: string;
-    passWord: string;
-}
--- dist/entity/user.d.ts
+++
@@ -1,9 +0,0 @@
-import { Login } from './login';
-export declare class User {
-    id: number;
-    firstName: string;
-    lastName: string;
-    country: string;
-    state: string;
-    login: Login;
-}
